efbb0aef962cec0580ca124e931e97fcbcdff173
Aurélien Houdart <aure.houdart@gmail.com>
2024-09-24 11:00:56 +0200

    fix : date desc / limit 50

--- objets/lib/API_call.dart
+++ objets/lib/API_call.dart
@@ -19,16 +19,15 @@ Future<DateTime?> getLastConnectionDate() async {
 }
 
 Future<List<dynamic>> fetchData(String? city, String? type, String datebefore, String dateafter) async {
-  String url = 'https://data.sncf.com/api/explore/v2.1/catalog/datasets/objets-trouves-restitution/records?where=date%20<%20"$dateafter"%20and%20date%20>%20"$datebefore"%20and%20gc_obo_date_heure_restitution_c%20is%20null&order_by=date%20desc&limit=100&offset=0&timezone=UTC&include_links=false&include_app_metas=false';
+  String url = 'https://data.sncf.com/api/explore/v2.1/catalog/datasets/objets-trouves-restitution/records?where=date%20<%20"$dateafter"%20and%20date%20>%20"$datebefore"%20and%20gc_obo_date_heure_restitution_c%20is%20null&order_by=date%20desc&limit=50&offset=0&timezone=UTC&include_links=false&include_app_metas=false';
   if (city != '' && type == 'Tous les types') {
-    url = 'https://data.sncf.com/api/explore/v2.1/catalog/datasets/objets-trouves-restitution/records?where=gc_obo_gare_origine_r_name%20like%20"$city"%20and%20date%20<%20"$dateafter"%20and%20date%20>%20"$datebefore"%20and%20gc_obo_date_heure_restitution_c%20is%20null&order_by=date%20desc&limit=100&offset=0&timezone=UTC&include_links=false&include_app_metas=false';
-  } else {
-    if (city != '' && type != 'Tous les types') {
-      url = 'https://data.sncf.com/api/explore/v2.1/catalog/datasets/objets-trouves-restitution/records?where=gc_obo_type_c%20%3D%20"$type"%20and%20gc_obo_gare_origine_r_name%20like%20"$city"%20and%20date%20<%20"$dateafter"%20and%20date%20>%20"$datebefore"%20and%20gc_obo_date_heure_restitution_c%20is%20null&order_by=date%20desc&limit=100&offset=0&timezone=UTC&include_links=false&include_app_metas=false';
-    }
+    url = 'https://data.sncf.com/api/explore/v2.1/catalog/datasets/objets-trouves-restitution/records?where=gc_obo_gare_origine_r_name%20like%20"$city"%20and%20date%20<%20"$dateafter"%20and%20date%20>%20"$datebefore"%20and%20gc_obo_date_heure_restitution_c%20is%20null&order_by=date%20desc&limit=50&offset=0&timezone=UTC&include_links=false&include_app_metas=false';
+  }
+  if (city != '' && type != 'Tous les types') {
+      url = 'https://data.sncf.com/api/explore/v2.1/catalog/datasets/objets-trouves-restitution/records?where=gc_obo_type_c%20%3D%20"$type"%20and%20gc_obo_gare_origine_r_name%20like%20"$city"%20and%20date%20<%20"$dateafter"%20and%20date%20>%20"$datebefore"%20and%20gc_obo_date_heure_restitution_c%20is%20null&order_by=date%20desc&limit=50&offset=0&timezone=UTC&include_links=false&include_app_metas=false';
   }
   if (city == '' && type != 'Tous les types') {  
-    url = 'https://data.sncf.com/api/explore/v2.1/catalog/datasets/objets-trouves-restitution/records?where=gc_obo_type_c%20%3D%20"$type"%20and%20date%20<%20"$dateafter"%20and%20date%20>%20"$datebefore"%20and%20gc_obo_date_heure_restitution_c%20is%20null&order_by=date%20desc&limit=100&offset=0&timezone=UTC&include_links=false&include_app_metas=false';
+    url = 'https://data.sncf.com/api/explore/v2.1/catalog/datasets/objets-trouves-restitution/records?where=gc_obo_type_c%20%3D%20"$type"%20and%20date%20<%20"$dateafter"%20and%20date%20>%20"$datebefore"%20and%20gc_obo_date_heure_restitution_c%20is%20null&order_by=date%20desc&limit=50&offset=0&timezone=UTC&include_links=false&include_app_metas=false';
   }
   final response = await http.get(Uri.parse(url));
   print("JSON");
@@ -47,7 +46,7 @@ Future<List<dynamic>> fetchData(String? city, String? type, String datebefore, S
 }
 
 Future<List<dynamic>> fetchDataSinceLastConnection(String datebefore) async {
-  final response = await http.get(Uri.parse('https://data.sncf.com/api/explore/v2.1/catalog/datasets/objets-trouves-restitution/records?where=date%20>%20"$datebefore"%20and%20gc_obo_date_heure_restitution_c%20is%20null&order_by=date%20desc&limit=100&offset=0&timezone=UTC&include_links=false&include_app_metas=false'));
+  final response = await http.get(Uri.parse('https://data.sncf.com/api/explore/v2.1/catalog/datasets/objets-trouves-restitution/records?where=date%20>%20"$datebefore"%20and%20gc_obo_date_heure_restitution_c%20is%20null&order_by=date%20desc&limit=50&offset=0&timezone=UTC&include_links=false&include_app_metas=false'));
   print("JSON");
   print(response.body);
 
